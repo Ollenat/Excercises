02965a1d3507b2faf2eca201d9ff781c7d2be7ba
Olle Björk <Olle@nbjork.se>
2022-07-15 14:57:43 +0200

    First test of the program (with all files)

---
+++ AnimalList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnimalList
+{
+    class Program
+    {
+        private int iterations = 5; 
+
+        private List<string> AnimalList = new List<string>();
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                Console.Write("Please name an animal: ");
+
+                string animal = Console.ReadLine();
+                AnimalList.Add(animal);
+            }
+            
+            foreach (var Animal in AnimalList)
+            {
+                Console.WriteLine(Animal);
+            }
+        }
+    }
+}
