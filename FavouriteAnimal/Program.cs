3193bb8a746d98fec0c75def0174cfa67800c430
Olle Björk <Olle@nbjork.se>
2022-07-16 13:53:15 +0200

    Made new console application for favourite animal excercise.

---
+++ FavouriteAnimal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FavouriteAnimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
